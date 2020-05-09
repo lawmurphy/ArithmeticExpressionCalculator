8893e40c2951006f28207a396f1c8e2f8da5110e
lawMurphy <lawmurphy@mail.ru>
2020-05-09 18:03:48 +0300

    Был обновлен дизайн приложения. Добавлены файлы rounded и rounded_blue.xml Добавил separator между двумя relativeLayout Убрана верхняя панель в приложении Обновил файл апк

--- app/src/main/java/shirokov/apps/arithmeticexpressioncalculator/MainActivity.java
+++ app/src/main/java/shirokov/apps/arithmeticexpressioncalculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import java.util.EmptyStackException;
